352592b8dd4e17ba62c9e727582d70b6da565cad
sylfort <sylviomafort@gmail.com>
2023-12-31 15:38:56 +0900

    Add basic routes, views, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+author = Author.create(name: "Raphael Jambalos", kind: "Programmer")
+post = Post.create(title: "Redis", body: "This is a in-memory database often used for caching.", author_id: author.id)
+post = Post.create(title: "PostgreSQL", body: "This is a transactional database used for transactions", author_id: author.id)
+post = Post.create(title: "DynamoDB", body: "This is a NoSQL database used for concurrent workloads.", author_id: author.id)
